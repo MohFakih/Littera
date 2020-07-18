09da323dc08c44b2c7f569909bf0153aff1507b6
Mohamad Fakih <mohfakih2000@gmail.com>
2020-07-19 01:23:38 +0300

    Implemented TensorFlow dataset pipeline. Removed old method of loading up images from drive.

--- imageLoad.py
+++ imageLoad.py
@@ -1,6 +1,3 @@
-import os
-import numpy as np
-from PIL import Image
 import tensorflow as tf
 
 """
@@ -8,61 +5,26 @@ Contains functions used in loading images into memory
 """
 
 
-
-# TODO: change this system to use tf.data.dataset and/or keras preprocessing, they probably have better data pipelining
-
-
-
-def getLanguageDirPaths():
-    """
-    :return: List of paths for the language folders (each language folder presumably contains sorted letter folders).
-    """
-    cyrillicPath = ".\letters\Cyrillic"
-    latinPath = ".\letters\Latin"
-    pathList = [latinPath, cyrillicPath]
-    return pathList
-
-def getImagesFromLanguageDir(languagePath):
-    """
-    :param languagePath: OS path to language dir. Dir should contain sub-directories for each letter images.
-    :return: list of paths for all the images in the sub-directories
-    """
-    imgPathList = []  # Holds path for each individual image
-    languageDirList = os.listdir(languagePath)  # List all letter directories
-    for letterDir in languageDirList:
-        pathToLetterDir = os.path.join(languagePath, letterDir)
-        imageNameList = os.listdir(pathToLetterDir)
-        imgPathList += [os.path.join(pathToLetterDir, imageName) for imageName in imageNameList]
-    return imgPathList
+def process_img(filename):
+    img = tf.io.read_file(filename)
+    img = tf.image.decode_png(img)
+    img = tf.image.resize(img, (256, 256))
+    img = img[:, :, 3]
+    img = tf.expand_dims(img, 2)
+    img = img / 255
+    return img
 
 
-def getPathToAllImages():
-    """
-    :return: List of paths to each individual image for all languages.
-    """
-    pathToAllImages = []
-    for langPath in getLanguageDirPaths():
-        pathToAllImages += getImagesFromLanguageDir(langPath)
-    return pathToAllImages
+class ImagePipeline:
+    def __init__(self, letterPath=".\\letters\\*.png", batch_size=32):
+        self.letterPath = letterPath
+        self.batch_size = batch_size
+        filename_ds = tf.data.Dataset.list_files(self.letterPath)
+        self.image_ds = filename_ds.map(process_img)
+        self.batched_image_ds = self.image_ds.batch(self.batch_size)
 
-def sampleImages(listOfPaths, n=512, dim1=None, dim2=None, exclude=None):
-    """
-    :param listOfPaths: List of paths to all the images to sample from
-    :param n: Number of images to sample
-    :param exclude: Number of images to exclude from the sampling (presumably to keep a validation set)
-    :param dim1: dim1 and dim2 are the dimensions of the output images, a None value will not resize the images
-    :param dim2: dim1 and dim2 are the dimensions of the output images, a None value will not resize the images
-    :return: (n, dim1, dim2, 1) numpy array holding n [dim1 * dim2] grayscale images
-    """
-    if not exclude:
-        pathsOfImagesToLoad = np.random.choice(listOfPaths, n, replace=False)
-    else:
-        pathsOfImagesToLoad = np.random.choice(listOfPaths[:-exclude], n, replace=False)
-    o = []
-    for path in pathsOfImagesToLoad:
-        img = np.array(Image.open(path))
-        dim_x = dim1 if dim1 else img.shape[0]
-        dim_y = dim2 if dim2 else img.shape[1]
-        img = img[:dim_x, :dim_y, 3].reshape((dim_x, dim_y, 1))
-        o.append(img / 255)
-    return np.array(o)
+    def get_batch(self):
+        return next(self.batched_image_ds.take(1).as_numpy_iterator())
+
+    def get_single(self):
+        return next(self.image_ds.take(1).as_numpy_iterator())
