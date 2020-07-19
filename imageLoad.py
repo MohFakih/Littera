bc7ecfc0f76f8ebcfdf78556116bb05eb262704a
Mohamad Fakih <mohfakih2000@gmail.com>
2020-07-19 23:22:15 +0300

    Changes to image pipeline to allow resizing of images to arbitrary dimensions.

--- imageLoad.py
+++ imageLoad.py
@@ -1,27 +1,26 @@
 import tensorflow as tf
 
-"""
-Contains functions used in loading images into memory
-"""
-
-
-def process_img(filename):
-    img = tf.io.read_file(filename)
-    img = tf.image.decode_png(img)
-    img = tf.image.resize(img, (256, 256))
-    img = img[:, :, 3]
-    img = tf.expand_dims(img, 2)
-    img = img / 255
-    return img
-
 
 class ImagePipeline:
-    def __init__(self, letterPath=".\\letters\\*.png", batch_size=32):
+    """
+    TensorFlow pipeline for loading images and batching them
+    """
+    def __init__(self, letterPath=".\\letters\\*.png", batch_size=32, img_size=(256, 256)):
+        self.img_size = img_size
         self.letterPath = letterPath
         self.batch_size = batch_size
         filename_ds = tf.data.Dataset.list_files(self.letterPath)
-        self.image_ds = filename_ds.map(process_img)
+        self.image_ds = filename_ds.map(self.process_img)
         self.batched_image_ds = self.image_ds.batch(self.batch_size)
+
+    def process_img(self, filename):
+        img = tf.io.read_file(filename)
+        img = tf.image.decode_png(img)
+        img = tf.image.resize(img, self.img_size)
+        img = img[:, :, 3]
+        img = tf.expand_dims(img, 2)
+        img = img / 255
+        return img
 
     def get_batch(self):
         return next(self.batched_image_ds.take(1).as_numpy_iterator())
